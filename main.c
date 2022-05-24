df38c470f9f76699179a938c90a44b5dd8d7f9f2
МоёИмя Фамилия <test@email.com>
2022-05-25 00:33:56 +0300

    add file

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <iostream>
+
+def summation(int first, int second){
+	return first + second;
+}
+
+def subtraction(int first, int second){
+	return first - second;
+}
+
+
+
+int int main(int argc, char const *argv[])
+{
+	int test_int = 25;
+
+	std::cout << summation(13,24) <<"\n";
+	std::cout << subtraction(13,24) <<"\n";
+
+	return 0;
+}
